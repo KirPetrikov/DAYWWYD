159ec418f152d148d0ce9c73d1f3a6bb4f8feac5
PKbioscience <bioscience.kp@gmail.com>
2023-10-06 23:24:53 +0300

    Update OperNA.py

--- src/OperNA.py
+++ src/OperNA.py
@@ -58,13 +58,14 @@ def process_na(option: str, seqs: list) -> list:
         "comp": complement_seq,
         "revcomp": reverse_complement_seq,
     }
-    if not (option in {"trans", "rev", "comp", "revcomp"}):
-        raise ValueError('Invalid operation!')
     if not (isinstance(seqs, list)):
         raise ValueError('Invalid input!')
     for seq in seqs:
         sequence_check(seq)
-    result_sequences = []
-    for seq in seqs:
-        result_sequences.append(options_dict[option](seq))
-    return result_sequences
+    if option in {"trans", "rev", "comp", "revcomp"}:
+        result_sequences = []
+        for seq in seqs:
+            result_sequences.append(options_dict[option](seq))
+        return result_sequences
+    else:
+        raise ValueError('Invalid operation!')
