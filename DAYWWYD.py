5c761b26720be269f4121bf639ebca4082c29d53
PKbioscience <bioscience.kp@gmail.com>
2023-10-07 20:27:29 +0300

    Edit DAYWWYD: rename OperNA and ProtSeqO

--- DAYWWYD.py
+++ DAYWWYD.py
@@ -1,8 +1,10 @@
 """"Provides different operations with bioinformatics data
 """
 
-import src.OperNA as nas
-import src.ProtSeqO as ps
+import src.na_seq_tool as nas
+import src.prot_seq_tool as ps
+import src.fastq_filter as ff
+
 
 def process_na(option: str, seqs: list) -> list:
     """Function process list of nucleotide sequences
@@ -24,6 +26,7 @@ def process_na(option: str, seqs: list) -> list:
         return result_sequences
     else:
         raise ValueError("Invalid operation!")
+
 
 def process_prot(option: str, seqs: list) -> list:
     """Performs operations on amino acids sequences:
@@ -47,3 +50,19 @@ def process_prot(option: str, seqs: list) -> list:
         return results
     else:
         raise ValueError("Invalid operation!")
+
+
+def process_fastq(seqs: dict, gc_bounds: tuple = (20, 80), len_bounds: tuple = (0, 2 ** 32),
+                  quality_threshold: int = 0) -> dict:
+    gc_lower = ff.parse_intervals(gc_bounds)[0]
+    gc_upper = ff.parse_intervals(gc_bounds)[1]
+    len_lower = ff.parse_intervals(len_bounds)[0]
+    len_upper = ff.parse_intervals(len_bounds)[1]
+    selected_seqs = {}
+    for key, value in seqs.items():
+        gc_condtition_check = ff.is_seq_pass_gc_filter(value[0], gc_lower, gc_upper)
+        len_condtition_check = ff.is_seq_pass_len_filter(value[0], len_lower, len_upper)
+        phred__condition_check = ff.is_seq_pass_phred_filter(value[1], quality_threshold)
+        if gc_condtition_check and len_condtition_check and phred__condition_check:
+            selected_seqs[key] = seqs[key]
+    return selected_seqs
